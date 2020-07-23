fb0bdf52c5da7f4f552ee05c946a7e44c7e0ec7d
Yassire Elhani <yassire@makeandship.com>
2020-07-23 11:25:47 +0100

    secrets in vault (#5)
    * secrets in vault
    
    * secrets in vault

---
+++ static-embassy/vault/create-secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export VAULT_TOKEN=<VAULT_TOKEN>
+export ES_PASSWORD=<ES_PASSWORD>
+export KEYCLOAK_ADMIN_PASSWORD=<KEYCLOAK_ADMIN_PASSWORD>
+export AWS_ACCESS_KEY=<AWS_ACCESS_KEY>
+export AWS_SECRET_KEY=<AWS_SECRET_KEY>
+
+curl -X POST \
+  http://localhost:8200/v1/kv/mykrobe-dev \
+  -H 'Content-Type: application/json' \
+  -H 'X-Vault-Token: '"$VAULT_TOKEN"'' \
+  -d '{
+  "ES_PASSWORD": "'"$ES_PASSWORD"'"",
+  "KEYCLOAK_ADMIN_PASSWORD": "'"$KEYCLOAK_ADMIN_PASSWORD"'"",
+  "AWS_ACCESS_KEY": "'"$AWS_ACCESS_KEY"'"",
+  "AWS_SECRET_KEY": "'"$AWS_SECRET_KEY"'""
+}'
