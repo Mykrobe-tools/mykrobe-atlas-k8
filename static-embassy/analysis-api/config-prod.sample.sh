8a8353073b6541ade5c8ab666b5c35109db78922
Zhicheng-Liu <zl@ebi.ac.uk>
2021-04-23 17:26:10 +0100

    Update bigsi and analysis api images to fix bigsi search issues.

--- static-embassy/analysis-api/config-prod.sample.sh
+++ static-embassy/analysis-api/config-prod.sample.sh
@@ -9,10 +9,10 @@ export ATLAS_AUTH_SERVER="https://accounts.mykro.be/auth"
 export ATLAS_AUTH_CLIENT_ID="analysis-api"
 export ATLAS_AUTH_CLIENT_SECRET="<secret>"
 
-export ANALYSIS_API_IMAGE="eu.gcr.io/atlas-275810/mykrobe-atlas-analysis-api:c401c92"
-export ANALYSIS_API_WORKER_IMAGE="eu.gcr.io/atlas-275810/mykrobe-atlas-analysis-api-worker:c401c92"
+export ANALYSIS_API_IMAGE="eu.gcr.io/atlas-275810/mykrobe-atlas-analysis-api:481c819"
+export ANALYSIS_API_WORKER_IMAGE="eu.gcr.io/atlas-275810/mykrobe-atlas-analysis-api-worker:481c819"
 
-export BIGSI_AGGREGATOR_IMAGE="zhichengliu/bigsi-ebi-api:b13a619"
+export BIGSI_AGGREGATOR_IMAGE="zhichengliu/bigsi-ebi-api:2e01b85d"
 export BIGSI_IMAGE="zhichengliu/bigsi:c329b10"
 export BACKGROUND_VARIANTS_DB_IMAGE="eu.gcr.io/atlas-275810/mykrobe-atlas-background-variants-db:4302c8c"
 
